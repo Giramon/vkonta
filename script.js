4bb5fdddcb3753bd1df045ea4851b61ecfcd6a3c
Giramon <120495791+Giramon@users.noreply.github.com>
2023-11-22 23:03:23 +0500

    Update script.js

--- script.js
+++ script.js
@@ -6,12 +6,16 @@ let videoInput;
 
 let imgMask;
 let imgFace;
+let imgBor;
+let imgVolos
 
 let selected = -1;
 
 function preload(){
-    imgMask = loadImage("https://img.icons8.com/officel/2x/user.png");
-    imgFace = loadImage("https://img.icons8.com/dotty/2x/monster-face.png");
+    imgMask = loadImage("https://cdn.websites.hibu.com/276dc06e9a89410aae33183412705e35/DESKTOP/png/7972545.png");
+    imgFace = loadImage("https://static.vecteezy.com/system/resources/previews/001/202/652/original/beard-png.png");
+    imgBor = loadImage("https://cdn-icons-png.flaticon.com/512/9066/9066024.png");
+    imgVolos = loadImage("https://cdn-icons-png.flaticon.com/512/42/42055.png");
 }
 
 function setup(){
@@ -27,7 +31,7 @@ function setup(){
     videoInput.hide();
 
     const sel = createSelect();
-    const selectList = ['Mack', 'Face'];
+    const selectList = ['Mack', 'Face','Bor','Volos'];
     sel.option('Select filter', -1);
 
     for(let i = 0; i <selectList.length; i++) {
@@ -51,6 +55,8 @@ function draw() {
         case '-1': break;
         case '0': drawMask(); break;
         case '1': drawFace(); break;
+        case '2': drawBor(); break;
+        case '3':drawVolos();break;
     }
 }
 
@@ -59,9 +65,9 @@ function drawMask() {
     if (positions !== false) {
         push();
         const wx = Math.abs(positions[13][0] - positions[1][0]) * 1.2;
-        const wy = Math.abs(positions[7][1] - Math.min(positions[16][1], positions[20][1])) * 1.2;
+        const wy = Math.abs(positions[37][1] - Math.min(positions[16][1], positions[20][1])) * 1.2;
         translate(-wx/2, -wy/2);
-        image(imgMask, positions[62][0], positions[62][1], wx, wy);
+        image(imgMask, positions[33][0], positions[33][1], wx, wy);
         pop();
     }
 }
@@ -74,6 +80,30 @@ function drawFace() {
         const wy = Math.abs(positions[7][1] - Math.min(positions[16][1], positions[20][1])) * 1.2;
         translate(-wx/2, -wy/2);
         image(imgFace, positions[62][0], positions[62][1], wx, wy);
+        pop();
+    }
+}
+
+function drawVolos() {
+    const positions = faceTracker.getCurrentPosition();
+    if (positions !== false) {
+        push();
+        const wx = Math.abs(positions[13][0] - positions[1][0]) * 1.2;
+        const wy = Math.abs(positions[7][1] - Math.min(positions[16][1], positions[20][1])) * 1.2;
+        translate(-wx/2, -wy/2);
+        image(imgVolos, positions[62][0], positions[62][1], wx, wy);
+        pop();
+    }
+}
+
+function drawBor() {
+    const positions = faceTracker.getCurrentPosition();
+    if (positions !== false) {
+        push();
+        const wx = Math.abs(positions[44][0] - positions[50][0]) * 1.2;
+        const wy = Math.abs(positions[7][1] - Math.min(positions[44][1], positions[50][1])) * 1.5;
+        translate(-wx/2, -wy/2);
+        image(imgBor, positions[60][0], positions[60][1], wx, wy);
         pop();
     }
 }
